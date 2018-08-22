84a97bae55e78f3cb6eca1975962869385b238fd
Garbo Cheng <garbocheng93@gmail.com>
2018-08-21 22:38:00 -0700

    modified sound volume

--- dist/lib/bundle.js
+++ dist/lib/bundle.js
@@ -619,6 +619,7 @@ function sound(src){
   this.sound.setAttribute("preload", "auto");
   this.sound.setAttribute("controls", "none");
   this.sound.style.display = "none";
+  this.sound.volume = 0.01;
   document.body.appendChild(this.sound);
   this.play = function(){
       this.sound.play();
