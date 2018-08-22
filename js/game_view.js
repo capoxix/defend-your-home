ea3bdf673e7c5d400c6f41b13d5e88819c2be9c3
Garbo Cheng <garbocheng93@gmail.com>
2018-08-21 23:06:11 -0700

    still trying to figure out animation

--- js/game_view.js
+++ js/game_view.js
@@ -1,3 +1,9 @@
+// var fps = 30;
+// var now;
+// var then = Date.now();
+// var interval = 1000/fps;
+// var delta;
+
 class GameView {
   constructor(game, ctx){
     this.ctx = ctx;
@@ -37,7 +43,38 @@ class GameView {
     this.lastTime = time;
 
     requestAnimationFrame(this.animate.bind(this));
+
+
+
+  //   now = Date.now();
+  // delta = now - then;
+  //
+  // if (delta > interval) {
+  //
+  //     // update time stuffs
+  //
+  //     // Just `then = now` is not enough.
+  //     // Lets say we set fps at 10 which means
+  //     // each frame must take 100ms
+  //     // Now frame executes in 16ms (60fps) so
+  //     // the loop iterates 7 times (16*7 = 112ms) until
+  //     // delta > interval === true
+  //     // Eventually this lowers down the FPS as
+  //     // 112*10 = 1120ms (NOT 1000ms).
+  //     // So we have to get rid of that extra 12ms
+  //     // by subtracting delta (112) % interval (100).
+  //     // Hope that makes sense.
+  //
+  //     then = now - (delta % interval);
+  //     this.game.step(timeDelta);
+  //
+  //     // debugger;
+  //     this.game.draw(this.ctx);
+  //
+  //     // ... Code for Drawing the Frame ...
+  //   }
   }
+
 
   // stopAnimation(){
     // window.cancelAnimationFrame(window.animation);
